2da551d8f5bac45e27b2ad1e985ac12df58aad7e
Jesse <stolwijkjesse@gmail.com>
2017-03-13 21:45:35 +0100

    v2

--- WorldCupElo/Program.cs
+++ WorldCupElo/Program.cs
@@ -17,33 +17,40 @@ namespace WorldCupElo
 		public Run()
 		{
 			bool ended = false;
-			using(var fs = File.OpenRead(@"../../datahier.csv"))
+            using (var fs = File.OpenRead(@"datahier.csv"))
 			using(var reader = new StreamReader(fs))
 			{
-				while (!reader.EndOfStream)
-				{
-					var line = reader.ReadLine();
-					var values = line.Split(',');
-					if (!ended) {
-						if (values [0] == "EINDE") {
-							ended = true;
-						} else {
-							int elo;
-							if (int.TryParse (values [0], out elo)) {
-								countries.Add (new Country (values [0], elo));
-							} else {
-								countries.Add (new Country (values [0]));
-							}
-						}
-					} else {
-						Country a = (Country)countries.Find (r => r.name == values [0]);
-						Country b = (Country)countries.Find (r => r.name == values [1]);
-						float res = float.Parse(values[2]);
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var values = line.Split(',');
+                    if (!ended)
+                    {
+                        if (values[0] == "EINDE")
+                        {
+                            ended = true;
+                        }
+                        else {
+                            int elo;
+                            if (int.TryParse(values[0], out elo))
+                            {
+                                countries.Add(new Country(values[0], elo));
+                            }
+                            else {
+                                countries.Add(new Country(values[0]));
+                            }
+                        }
+                    }
+                    else {
+                        Country a = (Country)countries.Find(r => r.name == values[0]);
+                        Country b = (Country)countries.Find(r => r.name == values[1]);
+                        float res = float.Parse(values[2]);
 
-						playMatch (new Match(a,b,res));
-					}
+                        playMatch(new Match(a, b, res));
+                    }
 
-				}
+                }
+                
 			}
 			countries.Sort ((a, b) =>  -1*a.elo.CompareTo(b.elo));
 			int total = 0;
@@ -56,9 +63,11 @@ namespace WorldCupElo
 			float avg = total / length;
 			Console.WriteLine ("-----------------");
 			Console.WriteLine ("Avg: {0}",avg.ToString());
-		}
+            Console.ReadKey();
 
-		void playMatch(Match match){
+        }
+
+        void playMatch(Match match){
 			float Ea = 1/(1+10^((match.two.elo - match.one.elo)/400));
 			float Eb = 1 - Ea;
 			match.one.elo += (int)(k * (match.result - Ea));
